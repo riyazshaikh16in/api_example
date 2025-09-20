0dc11c55e639fc0d78efc4cf925a862e8a44b2ee
riyazshaikh16in <riyazshaikh16in@gmail.com>
2025-09-20 21:25:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,12 @@ import uvicorn
 app = FastAPI()
 
 @app.get("/add")
-
-def add(a, b):
-    """Add two numbers and return the result."""
-    result = float(a) + float(b)
-    return {"operation": "add", "a": a, "b": b, "result": result}   
+def add(x,y):
+    return float(x)+float(y)
 
 @app.get("/subtract")
-def subtract(a, b):
-    """Subtract b from a and return the result."""
-    result = float(a) - float(b)
-    return {"operation": "subtract", "a": a, "b": b, "result": result}
+def subtract(x,y):
+    return float(x)-float(y)
 
-# Main program
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9321)
-
-
